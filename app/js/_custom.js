504635265aa9333d966ef1326abaa09ef2c826b1
YakovSpb <yakov_dux@bk.ru>
2020-11-23 14:38:33 +0300

    Well done

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -14,6 +14,43 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 putStars()
 
+var mediaQuery1200 = window.matchMedia("screen and (max-width: 1200px)");
+mediaQuery1200.addListener(foo1200);
+foo1200(mediaQuery1200);
+
+function foo1200(mq) {
+    if(mq.matches){
+    	mobileMenu()
+    }
+}
+
+
+function mobileMenu() {
+	const butOpen = document.querySelector('.menu-item-has-children a')
+	const butClose = document.querySelector('.sub-menu .menu-item')
+	const mobileMenu = document.querySelector('.sub-menu')
+	
+	butOpen.addEventListener('click', function(event) {
+		event.preventDefault();
+		mobileMenu.classList.add('open-menu')
+	})
+
+	butClose.addEventListener('click', function() {
+			mobileMenu.classList.remove('open-menu')
+	})
+
+}
+
+
+
+//close mobile menu
+
+document.addEventListener('click', function(e) {
+	const mobileMenu = document.querySelector('.sub-menu')
+	if (e.target.hasClass !== 'sub-menu' && (!e.target.matches('.menu-item-has-children') && !e.target.parentElement.matches('.menu-item-has-children'))) {
+		mobileMenu.classList.remove('open-menu')
+	}
+})
 
 
 });
